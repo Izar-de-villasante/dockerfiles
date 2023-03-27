13b1b65b12a59b1c06652e04eb98cd0be1fe1dde
Izar-de-villasante <idevillasante@carrerasresearch.org>
2023-03-27 11:57:44 +0200

    commandline to execute in local ws

---
+++ run.sh
@@ -0,0 +1,47 @@
+
+
+#export RSTUDIO_WHICH_R=$(which R)
+#sudo singularity instance start --writable-tmpfs \
+#	    --net --network-args "portmap=8080:8787/tcp" $1 rstudio-serv
+
+mkdir -p -m 700 run tmp var-lib-rstudio-server
+cat > database.conf <<END
+provider=sqlite
+directory=~/containers/var-lib-rstudio-server
+END
+
+
+cat > rsession.sh <<END
+#!/bin/sh
+export R_LIBS_USER=${HOME}/R/rocker-rstudio/4.2
+exec /usr/lib/rstudio-server/bin/rsession "\${@}"
+END
+
+chmod +x rsession.sh
+
+# #export APPTAINERENV_RSTUDIO_SESSION_TIMEOUT=0
+# #export APPTAINERENV_BIND="run:/run,var-lib-rstudio-server:/var/lib/rstudio-server,database.conf:/etc/rstudio/database.conf" 
+
+# "run:/run,tmp:/tmp,database.conf:/etc/rstudio/database.conf,rsession.sh:/etc/rstudio/rsession.sh,var-lib-rstudio-server:/var/lib/rstudio-server"
+# export APPTAINERENV_PASSWORD=$password
+
+
+
+#singularity exec --cleanenv $1 \
+#	    /usr/lib/rstudio-server/bin/rserver --www-port 8787  \
+#	    --auth-none=0 \
+#	    --auth-pam-helper-path=pam-helper \
+#	    --auth-stay-signed-in-days=30 \
+#	    --auth-timeout-minutes=0 \
+#	    --rsession-path=/etc/rstudio/rsession.sh \
+#	    --server-user=${USER}
+#
+#singularity exec   $1    /usr/lib/rstudio-server/bin/rserver --auth-none=1 --auth-pam-helper-path=pam-helper --server-user=${USER}
+#singularity exec --bind run:/run,var-lib-rstudio-server:/var/lib/rstudio-server,database.conf:/etc/rstudio/database.conf    $1    /usr/lib/rstudio-server/bin/rserver --auth-none=1 --auth-pam-helper-path=pam-helper --server-user=idevillasante
+
+
+PASSWORD='password' singularity exec --bind run:/run,containers/var-lib-rstudio-server:/var/lib/rstudio-server,database.conf:/etc/rstudio/database.conf containers/rstudio_4.2.sif /usr/lib/rstudio-server/bin/rserver --server-user=idevillasante
+
+printf 'rserver exited' 1>&2
+
+#singularity exec --bind   $1    /usr/lib/rstudio-server/bin/rserver --auth-none=1 --auth-pam-helper-path=pam-helper --server-user=${USER}  
