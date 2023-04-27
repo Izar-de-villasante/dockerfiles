1564bdde40e00d176a347a314abdf79c25f32c8b
Izar-de-villasante <idevillasante@carrerasresearch.org>
2023-04-27 14:25:58 +0200

    added cleanenv and run locally

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@
 #sudo singularity instance start --writable-tmpfs \
 #	    --net --network-args "portmap=8080:8787/tcp" $1 rstudio-serv
 
-mkdir -p -m 700 run tmp var-lib-rstudio-server
+mkdir -p -m 770 run tmp var-lib-rstudio-server
 cat > database.conf <<END
 provider=sqlite
-directory=~/containers/var-lib-rstudio-server
+directory=/var/lib/rstudio-server
 END
 
 
@@ -18,12 +18,14 @@ exec /usr/lib/rstudio-server/bin/rsession "\${@}"
 END
 
 chmod +x rsession.sh
-
+#export SINGULARITY_BIND="${workdir}/run:/run,${workdir}/tmp:/tmp,${workdir}/database.conf:/etc/rstudio/database.conf,${workdir}/rsession.sh:/etc/rstudio/rsession.sh,${workdir}/var/lib/rstudio-server:/var/lib/rstudio-server"
 # #export APPTAINERENV_RSTUDIO_SESSION_TIMEOUT=0
 # #export APPTAINERENV_BIND="run:/run,var-lib-rstudio-server:/var/lib/rstudio-server,database.conf:/etc/rstudio/database.conf" 
 
-# "run:/run,tmp:/tmp,database.conf:/etc/rstudio/database.conf,rsession.sh:/etc/rstudio/rsession.sh,var-lib-rstudio-server:/var/lib/rstudio-server"
-# export APPTAINERENV_PASSWORD=$password
+# local_vol:mounted_vol,
+
+# export APPTAINERENV_BIND="run:/run,tmp:/tmp,database.conf:/etc/rstudio/databse.conf,rsession.sh:/etc/rstudio/rsession.sh,var-lib-rstudio-server:/var/lib/rstudio-server"
+# export APPTAINERENV_PASSWORD=password
 
 
 
@@ -35,12 +37,13 @@ chmod +x rsession.sh
 #	    --auth-timeout-minutes=0 \
 #	    --rsession-path=/etc/rstudio/rsession.sh \
 #	    --server-user=${USER}
-#
-#singularity exec   $1    /usr/lib/rstudio-server/bin/rserver --auth-none=1 --auth-pam-helper-path=pam-helper --server-user=${USER}
-#singularity exec --bind run:/run,var-lib-rstudio-server:/var/lib/rstudio-server,database.conf:/etc/rstudio/database.conf    $1    /usr/lib/rstudio-server/bin/rserver --auth-none=1 --auth-pam-helper-path=pam-helper --server-user=idevillasante
+##
+##singularity exec   $1    /usr/lib/rstudio-server/bin/rserver --auth-none=1 --auth-pam-helper-path=pam-helper --server-user=${USER}
+##
+#singularity exec --bind run:/run,rsession.sh:/etc/rstudio/rsession.sh,var-lib-rstudio-server:/var/lib/rstudio-server,database.conf:/etc/rstudio/database.conf    $1    /usr/lib/rstudio-server/bin/rserver --auth-none=1 --auth-pam-helper-path=pam-helper --server-user=idevillasante --www-port 8788
 
-
-PASSWORD='password' singularity exec --bind run:/run,containers/var-lib-rstudio-server:/var/lib/rstudio-server,database.conf:/etc/rstudio/database.conf containers/rstudio_4.2.sif /usr/lib/rstudio-server/bin/rserver --server-user=idevillasante
+singularity exec ~/containers/dev.sif which R
+PASSWORD=pass singularity exec --cleanenv  --bind run:/run,var-lib-rstudio-server:/var/lib/rstudio-server,database.conf:/etc/rstudio/database.conf ~/containers/dev.sif /usr/lib/rstudio-server/bin/rserver --server-user=${USER} --auth-none=1 --auth-pam-helper-path=pam-helper --www-port 8788
 
 printf 'rserver exited' 1>&2
 
