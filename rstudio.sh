1298f72f12d0bb3b7f7bc53b3a698b74d9472825
ivillasante <152622613+ivillasante@users.noreply.github.com>
2024-01-15 15:25:05 +0100

    Update rstudio.sh
    made more readible

--- rstudio.sh
+++ rstudio.sh
@@ -1,12 +1,6 @@
-#!/bin/bash
-#
-# Loosely based on https://www.rocker-project.org/use/singularity/
-#
-# TODO:
-#  - Allow srun/sbatch from within the container.
-#
+#!/usr/bin/env bash
 
-#set -o xtrace
+# Loosely based on https://www.rocker-project.org/use/singularity/
 
 # This is an interesting image from somebody at dockerhub tags:{R_version}-{Seurat_version}
 # IMAGE=${IMAGE:-pansapiens/rocker-seurat:4.1.1-4.0.4}
@@ -19,7 +13,6 @@ IMAGE=${IMAGE:-rocker/rstudio:latest}
 
 # you can override the IMAGE in the script with the env command:
 #IMAGE=rocker/rstudio:4.1.1 ./rstudio.sh
-
 
 # Create the folders needed for rstudio to work:
 
@@ -51,6 +44,10 @@ chmod +x rsession.sh
 
 # Search for a free port to use:
 readonly PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
+if [ $? -ne 0 ]; then
+  # Set a default port value
+  PORT=8787
+fi
 
 #Generate the password:
 export SINGULARITYENV_PASSWORD=$(openssl rand -base64 15)
@@ -66,53 +63,43 @@ else
     HPC_ENV="local"
 fi
 
-
-#RSTUDIO_TMP=${HOME}/.rstudio-rocker/${IMAGE_SLASHED}/tmp
-## RSITELIB=${HOME}/.rstudio-rocker/${IMAGE_SLASHED}/site-library
-R_LIBS_USER=${HOME}/.rstudio-rocker/${IMAGE_SLASHED}
-#mkdir -p ${HOME}/.rstudio
-#mkdir -p ${RSITELIB}
-mkdir -p ${R_LIBS_USER}
-#mkdir -p ${RSTUDIO_TMP}
-#mkdir -p ${RSTUDIO_TMP}/var/run
-
-
 echo "Getting required containers ... this may take a while ..."
 echo
 # by doing `singularity test` we cache the container image without dumping a local sif file here
 # mksquashfs isn't installed everywhere, so we pull on a head node
 if [[ $HPC_ENV == "bbgn" ]]; then
-    # we use `singularity test` instead of `pull` to avoid leaving a .img file around
-    #ssh m3.massive.org.au bash -c "true && \
-    #                               module load singularity/${SINGULARITY_VERSION} && \
-    #                               singularity test docker://${IMAGE}"
     singularity test docker://${IMAGE}
+    LOCALPORT=8787
+    echo "On you local machine, open an SSH tunnel like:"
+    echo "  ssh -N -L ${LOCALPORT}:$(hostname -f):${PORT} -p 22022  ${USER}@bbgcluster"
+    echo
+    echo "Point your web browser at http://localhost:${LOCALPORT}"
+    echo
+    echo "Login to RStudio with:"
+    echo "  username: ${USER}"
+    echo "  password: ${SINGULARITYENV_PASSWORD}"
+    echo
+    echo "Protip: You can choose your version of R from any of the tags listed here: https://hub.docker.com/r/rocker/rstudio/tags"
+    echo "        and set the environment variable IMAGE, eg"
+    echo "        IMAGE=rocker/rstudio:4.1.1 $(basename "$0")"
+    echo
+    echo "Starting RStudio Server (R version from image ${IMAGE})"
+
 else
-    # pull to ensure we have the image cached
+    export LOCALPORT=$PORT
+    echo "Point your web browser at http://localhost:${LOCALPORT}"
+    echo
+    echo "Login to RStudio with:"
+    echo "  username: ${USER}"
+    echo "  password: ${SINGULARITYENV_PASSWORD}"
+    echo
+    echo "Protip: You can choose your version of R from any of the tags listed here: https://hub.docker.com/r/rocker/rstudio/tags"
+    echo "        and set the environment variable IMAGE, eg"
+    echo "        IMAGE=rocker/rstudio:4.1.1 $(basename "$0")"
+    echo
+    echo "Starting RStudio Server (R version from image ${IMAGE})"
     singularity pull docker://${IMAGE}
 fi
-
-
-echo
-
-LOCALPORT=8787
-PUBLIC_IP=$(curl https://checkip.amazonaws.com)
-
-echo "On you local machine, open an SSH tunnel like:"
-# echo "  ssh -N -L ${LOCALPORT}:localhost:${PORT} ${USER}@m3-bio1.erc.monash.edu.au"
-echo "  ssh -N -L ${LOCALPORT}:$(hostname -f):${PORT} -p 22022  ${USER}@bbgcluster"
-echo
-echo "Point your web browser at http://localhost:${LOCALPORT}"
-echo
-echo "Login to RStudio with:"
-echo "  username: ${USER}"
-echo "  password: ${SINGULARITYENV_PASSWORD}"
-echo
-echo "Protip: You can choose your version of R from any of the tags listed here: https://hub.docker.com/r/rocker/rstudio/tags"
-echo "        and set the environment variable IMAGE, eg"
-echo "        IMAGE=rocker/rstudio:4.1.1 $(basename "$0")"
-echo
-echo "Starting RStudio Server (R version from image ${IMAGE})"
 
 # Set some locales to suppress warnings
 LC_CTYPE="C"
@@ -121,29 +108,22 @@ LC_MONETARY="C"
 LC_PAPER="C"
 LC_MEASUREMENT="C"
 
-
-
-
-#if [[ $HPC_ENV == 'bbgn' ]]; then
-    #SINGULARITY_BIND="/tmp,/workspace,/scratch,${HOME}:/home/rstudio,${RSTUDIO_HOME}:${HOME}/.rstudio,${R_LIBS_USER}:${R_LIBS_USER},${RSTUDIO_TMP}:/tmp,${RSTUDIO_TMP}/var:/var/lib/rstudio-server,${RSTUDIO_TMP}/var/run:/var/run/rstudio-server,database.conf:/etc/rstudio/database.conf"
-export SINGULARITY_BIND="${RSTUDIO_HOME}:${HOME}/.rstudio,${RSTUDIO_TMP}:/tmp,${RSTUDIO_TMP}/var:/var/lib/rstudio-server,${RSTUDIO_TMP}/var/run:/var/run/rstudio-server,/workspace,${R_LIBS_USER},rsession.sh:/etc/rstudio/rsession.sh,database.conf:/etc/rstudio/database.conf,${RSITELIB}:/usr/local/lib/R/site-library"
-export SINGULARITYENV_RSTUDIO_SESSION_TIMEOUT=0
-
+if [[ $HPC_ENV == 'bbgn' ]]; then
+    export SINGULARITY_BIND="${RSTUDIO_HOME}:${HOME}/.rstudio,${RSTUDIO_TMP}:/tmp,${RSTUDIO_TMP}/var:/var/lib/rstudio-server,${RSTUDIO_TMP}/var/run:/var/run/rstudio-server,/workspace,${R_LIBS_USER},rsession.sh:/etc/rstudio/rsession.sh,database.conf:/etc/rstudio/database.conf,${RSITELIB}:/usr/local/lib/R/site-library"
+    export SINGULARITYENV_RSTUDIO_SESSION_TIMEOUT=0
     singularity exec --env R_LIBS_USER=${R_LIBS_USER} \
     docker://${IMAGE} \
                      /usr/lib/rstudio-server/bin/rserver --server-user=${USER} --auth-none=0 --auth-pam-helper-path=pam-helper --auth-timeout-minutes=0 --auth-stay-signed-in-days=30 --rsession-path=/etc/rstudio/rsession.sh  --www-port=${PORT} 
-#else
-#    SINGULARITYENV_PASSWORD="${PASSWORD}" \
-#    singularity exec --bind ${HOME}:/home/rstudio \
-#                     --bind ${RSTUDIO_HOME}:${HOME}/.rstudio \
-#                     --bind ${R_LIBS_USER}:${R_LIBS_USER} \
-#                     --bind ${RSTUDIO_TMP}:/tmp \
-#                     --bind=${RSTUDIO_TMP}/var:/var/lib/rstudio-server \
-#                     --bind=${RSTUDIO_TMP}/var/run:/var/run/rstudio-server \
-#                     --env R_LIBS_USER=${R_LIBS_USER} \
-#                     docker://${IMAGE} \
-#                     rserver --database-config-file database.conf --auth-none=0 --auth-pam-helper-path=pam-helper --www-port=${PORT}
-#                     # --bind ${RSITELIB}:/usr/local/lib/R/site-library \
-#fi
+else
+    singularity exec --bind ${HOME}:/home/rstudio \
+                     --bind ${RSTUDIO_HOME}:${HOME}/.rstudio \
+                     --bind ${R_LIBS_USER}:${R_LIBS_USER} \
+                     --bind ${RSTUDIO_TMP}:/tmp \
+                     --bind=${RSTUDIO_TMP}/var:/var/lib/rstudio-server \
+                     --bind=${RSTUDIO_TMP}/var/run:/var/run/rstudio-server \
+                     --env R_LIBS_USER=${R_LIBS_USER} \
+                     docker://${IMAGE} \
+                     rserver --database-config-file database.conf --auth-none=0 --auth-pam-helper-path=pam-helper --www-port=${PORT}
+fi
 
 printf 'rserver exited' 1>&2
